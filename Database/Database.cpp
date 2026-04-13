28381d8c501e80018314b76b08844dd00f956695
superchickeny <94999486+superchickeny@users.noreply.github.com>
2026-04-13 02:39:42 -0400

    insert added

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "db_main.h"
 #include "httplib.h"
+#include "table.h"
 
 int main()
 {
-    db_main db{};
-    std::cout << db.tables.size();
+    table<std::string> table{ {"dog"}};
+    auto res = table.insert("da", "dog", "balls");
+    std::cout << res.first << " " << res.second;
+
+  
 }
--- Database/db_main.h
+++ Database/db_main.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <vector>
-#include "table.h"
+#include "table_base.h"
 
 struct db_main
 {
 
+	static inline db_main* instance = nullptr;
 	std::vector<table_base> tables;
 
+	static const db_main* get_instance();
 };
