3347d7bcba07b63f15ac45798ebc1eb4a6b4a8d9
superchickeny <94999486+superchickeny@users.noreply.github.com>
2026-04-13 02:39:42 -0400

    insert added

--- Database/db_main.cpp
+++ Database/db_main.cpp
@@ -1,1 +1,10 @@
 #include "db_main.h"
+
+const db_main* db_main::get_instance()
+{
+    if (db_main::instance == nullptr)
+    {
+        db_main::instance = new db_main();
+    }
+    return db_main::instance;
+}
