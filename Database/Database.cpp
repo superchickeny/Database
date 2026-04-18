f03f69d17985c639cc8d359cb78374837028dfbc
Shane O'Connell <ocon0316@algonquinlive.com>
2026-04-17 22:10:32 -0400

    changed table names

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -5,16 +5,14 @@ int main()
 {
 	db_main* db = db_main::get_instance();
 	db->start_data_persistance_thread();
-	db->start_server_thread(); //shone ur shit is in here
-
-	db->add_table("users", {"userID", "retard", "taco", "burrito"});
+	db->start_server_thread();
+	db->add_table("users", {"userID", "taco", "burrito"});
 	table* users = db->get_table("users");
 
 	db_util::timed_event([users]() {
 			for (int i = 0; i < 2000; i++)
 			{
 				users->insert(std::to_string(i), "userID", std::to_string(i));
-				users->insert(std::to_string(i), "retard", std::to_string(i + 100));
 				users->insert(std::to_string(i), "taco", std::to_string(i + 10320020));
 				users->insert(std::to_string(i), "burrito", std::to_string(i + 10000));
 			}		
